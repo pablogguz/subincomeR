2904e01051643e062befbadc8b5ed94afa9f551f
pablo <72149928+pablogguz@users.noreply.github.com>
2024-10-31 11:53:39 +0000

    housekeeping

--- data-raw/_prepare_release.R
+++ data-raw/_prepare_release.R
@@ -14,11 +14,3 @@ pkgdown::build_site()
 
 # Package version ----
 usethis::use_version()
-
-# Create GitHub release
-usethis::use_github_release()
-
-# Create git tag
-new_version <- desc::desc_get_version()
-repo <- git2r::repository(".")
-git2r::tag(repo, paste0("v", new_version))
